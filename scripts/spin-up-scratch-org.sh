5427e085ce389ed79ca7285c36716e40e63d5eea
junliu <jun.liu@jmcloudservices.com>
2024-06-27 19:34:51 +1000

    update script

--- scripts/spin-up-scratch-org.sh
+++ scripts/spin-up-scratch-org.sh
@@ -4,7 +4,7 @@
 echo "Creating a new scratch org..."
 sf org create scratch --definition-file config/project-scratch-def.json --alias SnowfakeryDemo --target-dev-hub JM_PBO
 
-## Step 2: Push code
+## Step 2: deploy code
 sf project deploy start  --target-org SnowfakeryDemo
 
 # Step 3: Generate fake data using Snowfakery
@@ -12,10 +12,9 @@ echo "Generating fake data..."
 snowfakery snowfakery/Account_Contact_Case_Opportunity_Role_Reservation.yml --output-format csv --output-folder test-data/
 
 # Step 4: Use the Python script to prepare the data for upload
-echo "Preparing data for upload..."
+echo "Preparing data and Generating export.json for upload..."
 python3 scripts/generate_export_json.py test-data
 
 # Step 5: Upload the data to the scratch org using SFDMU
 echo "Uploading data to the scratch org..."
-cd test-data/
-sf sfdmu run --sourceusername csvfile  --targetusername SnowfakeryDemo
+sf sfdmu run --sourceusername csvfile  --targetusername SnowfakeryDemo --path test-data
